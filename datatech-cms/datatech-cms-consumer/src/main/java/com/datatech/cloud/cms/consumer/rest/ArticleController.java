27e54cbd3eb89bd0b2da256d61fbb9884f5b5520
DP <xushifu@live.cn>
2019-04-18 17:27:41 +0800

    cms add

--- datatech-cms/datatech-cms-consumer/src/main/java/com/datatech/cloud/cms/consumer/rest/ArticleController.java
+++ datatech-cms/datatech-cms-consumer/src/main/java/com/datatech/cloud/cms/consumer/rest/ArticleController.java
@@ -7,13 +7,19 @@ import com.datatech.cloud.cms.entity.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
 
 
 @Controller
 @RequestMapping("article")
 public class ArticleController extends BaseController<ArticleBiz,Article> {
-    public String hellocms(@PathVariable("username") String username){
-        return "hello datatech cms";
+
+    @RequestMapping(value = "/datatech", method = RequestMethod.GET)
+    public @ResponseBody
+    String getAllMenus() throws Exception {
+        return "hello datatech";
     }
 }
