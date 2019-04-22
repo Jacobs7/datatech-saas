5015fb5695f94834502e73d315d6d3af12dbb14c
747485114@qq.com <zly19951216>
2019-04-22 17:47:09 +0800

    商城信息管理-zly

---
+++ datatech-shop/datatech-shop-provider/src/main/java/com/datatech/springcloud/shop/biz/LocaltionBiz.java
@@ -0,0 +1,19 @@
+package com.datatech.springcloud.shop.biz;
+
+import com.datatech.cloud.common.biz.BaseBiz;
+import com.datatech.springcloud.shop.entity.Localtion;
+import com.datatech.springcloud.shop.mapper.LocaltionMapper;
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * 
+ *
+ * @author Datatech.Co
+ * @email 23484376@qq.com
+ * @date 2019-04-22 14:39:55
+ */
+@Service
+public class LocaltionBiz extends BaseBiz<LocaltionMapper, Localtion> {
+}
---
+++ datatech-shop/datatech-shop-provider/src/main/java/com/datatech/springcloud/shop/mapper/DetailMapper.java
@@ -0,0 +1,17 @@
+package com.datatech.springcloud.shop.mapper;
+
+
+import com.datatech.springcloud.shop.entity.Detail;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 
+ * 
+ * @author Datatech.Co
+ * @email 23484376@qq.com
+ * @date 2019-04-22 14:39:55
+ */
+
+public interface DetailMapper extends Mapper<Detail> {
+	
+}
